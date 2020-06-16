a55e19e6baa310a8d0386a3ef4394764e69fb531
Yuta Fukasawa <yuta.fukasawa28@gmail.com>
2020-06-16 15:14:12 +0900

    step01

---
+++ step01.py
@@ -0,0 +1,13 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+
+if __name__ == "__main__":
+    import numpy as np
+    data = np.array(1.0)
+    x = Variable(data)
+    print(x.data)
+
+    x.data = np.array(2.0)
+    print(x.data)
