b4dd97fdec1ddd6330e41cb8dd926b4345eef269
Yuta Fukasawa <yuta.fukasawa28@gmail.com>
2020-06-19 14:14:22 +0900

    step03

---
+++ step03.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from step01 import Variable
+from step02 import Function, Square
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
+if __name__ == '__main__':
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+    print(y.data)
